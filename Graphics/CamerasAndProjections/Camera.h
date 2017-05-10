5f7701c16e099c83f37ab3779eece6b9d544ab9a
Eugene Hochenedel <eugeneatschool@gmail.com>
2017-05-10 12:57:03 -0500

    Cameras and Projections
    Start of the Cameras and Projection tutorial.
    Began experimenting with the orthographic projections.

---
+++ Graphics/CamerasAndProjections/Camera.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include "src\gl_core_4_4.h"
+#include <GLFW\glfw3.h>
+#include "src\Gizmos.h"
+#include <glm.hpp>
+#include <ext.hpp>
+#include <gtc\matrix_transform.hpp>
+#include <gtx\transform.hpp>
+
+class Camera
+{
+public:
+	virtual void update(float deltaTime) = 0;
+	void setPerspective(float fieldOfView, float aspectRatio, float near, float far);
+	void setLookAt(glm::vec3 from, glm::vec3 to, glm::vec3 up);
+	void setPosition(glm::vec3 position);
+	glm::mat4 getWorldTransform();
+	glm::mat4 getView();
+	glm::mat4 getProjection();
+	glm::mat4 getProjectionView();
+
+private:
+	glm::mat4 worldTransform;
+	glm::mat4 viewTransform;
+	glm::mat4 projectionTransform;
+	glm::mat4 projectionViewTransform;
+	void updateProjectionViewTransform();
+};
